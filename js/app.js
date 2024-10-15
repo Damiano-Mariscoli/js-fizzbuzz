44fed6cbe73bb74b50dc8d854c19cf9443048ad8
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-15 16:06:48 +0200

    FIX: pseudo codice

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//definire il cilo for
+//definire il ciclo for
 //il ciclo for si ripeterà per 100 volte andando avanti sempre di 1 posto
 //poniamo la condizione straordinaria come primo -if-, se entrambi i risultati sono identici nella stessa posizione di i allora la condizione si attiva
 //scriviamo il resto delle condizioni "fizz" "buzz". per "fizz" se il modulo di 3 è uguale a 0 e per "buzz" se il modulo di 5 è uguale a 0
@@ -8,14 +8,14 @@
 
 for (let i = 0; i<100; i++)  {
     const num = i +1
-    if(num % 3== 0 && num%5==0){
+    if(num % 3 == 0 && num % 5== 0){
         console.log(num,'FizzBuzz');
     }
-    else if(check3 = num % 3 == 0){
+    else if(num % 3 == 0){
         console.log(num, 'Fizz')
     }
-    else if(check5 = num % 5 == 0){
-        console.log(num,'Buzz' )
+    else if(num % 5 == 0){
+        console.log(num,'Buzz')
     }
     else{
         console.log(num)

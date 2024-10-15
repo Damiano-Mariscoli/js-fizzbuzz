e0664cfa827d09ce67607d80c5b3e074867f9385
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-15 15:40:02 +0200

    FIX: descrizione più dettagliata

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //definire il cilo for
 //il ciclo for si ripeterà per 100 volte andando avanti sempre di 1 posto
-//poniamo la condizione straordinaria come primo -if-
-//scriviamo il resto delle condizioni "fizz" "buzz"
+//poniamo la condizione straordinaria come primo -if-, se entrambi i risultati sono identici nella stessa posizione di i allora la condizione si attiva
+//scriviamo il resto delle condizioni "fizz" "buzz". per "fizz" se il modulo di 3 è uguale a 0 e per "buzz" se il modulo di 5 è uguale a 0
 //tutto quello che non viene considerato dai primi if verrà mostrato senza nessuna descrizione
 
 
